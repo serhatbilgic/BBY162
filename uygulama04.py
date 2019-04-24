4bfdaea0e53ae43ba4ad93652ac0168957bca5d5
Ardıl Serhat Bilgiç <36641898+serhatbilgic@users.noreply.github.com>
2019-04-24 14:14:35 +0300

    Add files via upload

--- uygulama04.py
+++ uygulama04.py
@@ -1,17 +1,37 @@
-erkekismi= input("Bir erkek ismi giriniz:")
-kadÄ±nismi= input("Bir bayan ismi giriniz:")
-dize= int(input("Dize sayÄ±sÄ± giriniz...Maksimum 10 mÄ±sra yazdÄ±rÄ±labilir.."))
+Python 2.7.1 (r271:86832, Nov 27 2010, 18:30:46) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #1.uygulama
 
+metin = "Açýk bilim, araþtýrma çýktýlarýna ve süreçlerine herkesin serbestçe eriþmesini, bunlarýn ortak kullanýmýný, daðýtýmýný ve üretimini kolaylaþtýran bilim uygulamasýdýr."
 
-sarkÄ± = ["Giden her sevgilinin ardÄ±ndan "," " + kadÄ±nismi+ " " ,"Hep biz olduk el sallayan ","HaykÄ±rsak duyarlar mÄ± sesimizi? ","Hangi sevdadan galip Ã§Ä±ktÄ±k ki? " ," " + erkekismi+ " ", "BoÅŸuna Ã§ekilmedi bunca Ã§ile "," " + kadÄ±nismi+ " ", "Ä°Ã§iyoruz gÃ¼ndÃ¼z gece ", " " + erkekismi+ " ", "HaykÄ±rdÄ±k ama duymadÄ± hiÃ§ kimse ","PeÅŸindeyiz heryerdeâ€¦ "," " + erkekismi+" ", "Zaten aÅŸklar hep yalan dolan "," "+ erkekismi+" ","Sonu hep acÄ± hÃ¼sran "]
+metin = list(metin)
+for i in range(20):
+    print(metin[i],end="")
+    
+>>> #2.uygulama
 
+liste = ["Açýk Bilim", "Açýk Eriþim", "Açýk Lisans", "Açýk Eðitim", "Açýk Veri", "Açýk Kültür"]
 
-for olusturulacak_sarkÄ± in sarkÄ±[:dize]:
-    print(olusturulacak_sarkÄ±)
+for i in metin :
+    print(i)
+    
+>>> #3.uygulama
 
-if dize > 10:
-    print("En az 10 tane dize giriniz...")
+sozluk = {“Elma” : “Aðaçta yetiþen bir tür meyve” , “Salatalýk” : “Fidan üzerinde büyüyen bir tür sebze” }
+arma = str(input("ne aramak istiyorsun?")
+try :
+    print(sozluk[arama])
+except KeyError :
+     print("Tekrar Deneyiniz")
 
+SyntaxError: invalid syntax
+>>> #3.uygulama
 
-
-
+sozluk = {"Elma" : "Aðaçta yetiþen bir tür meyve" , "Salatalýk" : "Fidan üzerinde büyüyen bir tür sebze" }
+arma = str(input("ne aramak istiyorsun?")
+try :
+    print(sozluk[arama])
+except KeyError :
+     print("Tekrar Deneyiniz")
+	   
+>>> 
